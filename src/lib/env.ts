133e64e922fe8b9a7f4a3dfa77eb00b8a1d77846
fariselpurhami <fares.alternate2014@gmail.com>
2026-05-11 10:45:43 +0300

    🚀 config: Updating env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -20,7 +20,7 @@ const serverSchema = z.object({
 
 const clientSchema = z.object({
   NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
-  NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1).optional(),
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
   NEXT_PUBLIC_APP_URL: z.string().url().optional(),
 });
 
